5bf154cd95aa00ac0a0602159fabce91fc776dab
vyenman1 <63924406+vyenman1@users.noreply.github.com>
2020-08-23 15:30:27 -0400

    Initial commit

---
+++ Test Suites/NewTestSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>23dff618-803c-454f-b49e-2e92d8306ee7</testSuiteGuid>
+</TestSuiteEntity>
